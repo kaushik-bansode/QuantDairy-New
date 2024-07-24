2a770cb3c81d9d1a0f634d96ef70349f784595ac
kaushik-bansode <kaushik.bansode@erpdata.in>
2024-07-24 17:59:50 +0530

    sock report removed

--- quantdairy/quantdairy/report/sock_balance_quantity/sock_balance_quantity.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, quantdairy and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Sock Balance Quantity"] = {
-	"filters": [
-
-	]
-};
